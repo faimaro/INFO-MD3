b22af9e7331210922e4a3c4f3570cf2b654129ce
faimaro <aimarofran@gmail.com>
2018-10-22 11:51:17 -0300

    empiezo

--- MT.py
+++ MT.py
@@ -1,28 +1,33 @@
+from tabulate import tabulate
 
 # x = raw_input("Ingrese la entrada [0,1]* : ")
 # f = raw_input("Ingrese las transiciones (ejemplo: 0 < a)")
 # from numpy import *
-
+# from tabulate import tabulate
 def crearMaquina():
-    estados = []
-    estados = input("Ingrese lista de estados (ej:['q1','FIN'] => ")
+    # estados = []
+    estados = eval(input("Ingrese lista de estados (ej:['q1','FIN'] => "))
     # a = 'FIN' in estados
     crearFuncion(estados)
        
 
 def crearFuncion ( q ):
-    alfa = ['0','1']
-    numero_filas = len(q)-1     #le resto el estado FIN
-    matriz = [range(2) for i in range(numero_filas)]
-    print (matriz)   
+    alfa = ['0','1']   
+    n_filas = len(q)-1 #le resto el estado FIN
+    m_col = 3
+    print (n_filas)   
+    a = [[0] * m_col for i in range(n_filas)]
+    print (a)   
     
-    for i in range (0,numero_filas):
-        e = q[i]
-        a = input("ingrese transicion estado {e}")
-        matriz[i][0] = a
-        b = input("ingrese transicion")
-        matriz[i][1] = b
-    print (matriz)
+    for i in range (0,n_filas):
+        # e = q[i]
+        a[i][0]= q[i]
+        e = input("ingrese transicion 0")
+        a[i][1] = e
+        b = input("ingrese transicion 1")
+        a[i][2] = b
+    
+    print(tabulate(a,headers=['0', '1']))
 
  
 # Menos intuitiva pero mas eficiente
@@ -34,7 +39,7 @@ crearMaquina()
 
 
 
-#implementacion de matrices en python : http://www.cristalab.com/tutoriales/crear-matrices-en-python-utilizando-listas-c103122l/
+#implementacion de matrices en python : https://snakify.org/es/lessons/two_dimensional_lists_arrays/
 
 
 
