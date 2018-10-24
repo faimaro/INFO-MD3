226027eeb25558442910cb1d3240ed6ae0981b1a
faimaro <aimarofran@gmail.com>
2018-10-24 12:41:41 -0300

    mejoras en el proceso

--- ejecucion de maquina.py
+++ ejecucion de maquina.py
@@ -4,61 +4,43 @@ import random
 
 def crearMaquina():
     # print(enumerarMaquinas(6)[0])
-    maq = enumerarMaquinas(3)
-    estados = maq[3]#['a','b','FIN']#eval(input("Ingrese lista de estados (ej:['q1','FIN'] => "))
-    print(maq[2])
-    print(estados)
-    trans = maq[2]
-    
-    #[['a', '1>b', '1<a'], ['b', '0<a', 'fin']] #crearTabla(estados)
-    # trans = [['a', 'fin', '1<a'], ['b', 'fin', 'fin']]
-    # trans = [['a', '1>b', '1<a'], ['b', '1>c', '0>c'], ['c', '1<a', 'fin']]
-    # trans = [['a', '1>b', '1<a'], ['b', '0<a', 'fin']]
-    cinta = crearCinta()
-    test = procesar(trans,cinta,estados,10) #pruebo la cantidad de pasos=3
-    print("kk",test)
-
-       
-# def crearTabla ( q ):
-#     n_filas = len(q)-1 #le resto el estado FIN
-#     m_col = 3
-#     print (n_filas)   
-#     a = [[0] * m_col for i in range(n_filas)]
-    
-#     for i in range (0,n_filas):
-#         a[i][0]= q[i]
-#         e1 = input("ingrese transicion Entrada: 0, Estado: "+q[i]+" : ")
-#         a[i][1] = e1
-#         e2 = input("ingrese transicion Entrada: 1, Estado: "+q[i]+" : ")
-#         a[i][2] = e2
-#     print(tabulate(a,headers=['0', '1'], tablefmt='fancy_grid')) #imprimo la MT como tabla
-#     # print(a)
-#     return (a)
+    est = eval(input("cuantos estados ? :"))
+    pasos = eval(input("cuantos pasos maximo ? :"))
+    maq = enumerarMaquinas(est)
+    for h in range(0,len(maq),2):#len(maq)):
+        pass
+        estados = maq[h+1]#['a','b','FIN']#eval(input("Ingrese lista de estados (ej:['q1','FIN'] => "))
+        trans = maq[h]
+        cinta = crearCinta()
+        test = procesar(trans,cinta,estados,pasos) #pruebo la cantidad de pasos=3
+        if (test!=None):
+            print(tabulate(test,headers=['0', '1'], tablefmt='fancy_grid')) #imprimo la MT como tabla
+        
     
 
 def crearCinta():    
     cinta = [] #lista de strings
     # for i in range (0,9):
     #     cinta.insert(0,'0')
-    for i in range (0,100):
+    for i in range (0,1000):
         cinta.append('0') 
     return (cinta)
 
 def procesar(trans,cinta_entrada,estados,cant_pasos):
     cinta = cinta_entrada
-    print(trans)
     cabezal = int(len(cinta)/2) #posicion del cabezal
     pasos = 0
     # fin = estados.pop()
-    print(estados)
     estado_str = estados[0] #estado inicial a
     estado_idx = 0 #estado inicial a
     
     #leo
-    while estado_str != "fin" or  estado_str != "FIN"  : 
+    while estado_str != "FIN": 
+        if pasos==cant_pasos:
+            break
         a = list()
-        if trans[estado_idx][0]=='fin' or trans[estado_idx][0]=='FIN':
-            return(pasos)       
+        # if trans[estado_idx][0]=='FIN':
+        #     return(None)       
         entrada = cinta[cabezal]
         if entrada=='0' and estado_str == trans[estado_idx][0]:
             pasos += 1
@@ -71,10 +53,11 @@ def procesar(trans,cinta_entrada,estados,cant_pasos):
                 cabezal +=1
             if a[1]=='<':
                 cabezal -= 1 
-            if a[2] != 'n' and a[2] != 'N' :   
+            if a[2] != 'N' :   
             # print(estado_idx)         
                 estado_str = a[2]
                 estado_idx = estados.index(a[2]) 
+            else:break
                  
 
         if entrada=='1' and estado_str == trans[estado_idx][0]:
@@ -87,20 +70,18 @@ def procesar(trans,cinta_entrada,estados,cant_pasos):
             if a[1]=='>':                
                 cabezal +=1
             if a[1]=='<':
-                cabezal -= 1            
-
-            if a[2] != 'n' and a[2] != 'N' :   
+                cabezal -= 1         
+            if a[2] != 'N' :
                 # print(estado_idx)         
                 estado_str = a[2]
                 estado_idx = estados.index(a[2])
-        else:break
-    # print(cinta)        
-              
+            else:break     
+                   
   
-    if pasos<=cant_pasos:
-        return(True)
+    if pasos<cant_pasos and pasos>1:
+        return(trans)
     else:
-        return(False)
+        return(None)
 
     
 def enumerarMaquinas(tam):
